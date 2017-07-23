7a8ed0b21b846fac62fa3d0ba560e73cbd234cbd
Emilien Klein <emilien@klein.st>
2017-07-23 15:28:03 +0200

    Upload script also calls `git push`

--- upload.sh
+++ upload.sh
@@ -1,1 +1,1 @@
-rsync -av -e 'ssh -p 21324' --exclude=temp/ --exclude=upload.sh ./* e2jk@pascal.klein.st:/var/www/html/vallenato.fr/aprender
+git push && rsync -av -e 'ssh -p 21324' --exclude=temp/ --exclude=upload.sh ./* e2jk@pascal.klein.st:/var/www/html/vallenato.fr/aprender
