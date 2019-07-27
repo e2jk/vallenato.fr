8b0ac235b9d7ba52c05b495fd6e73e65870e873e
Emilien Klein <emilien@klein.st>
2019-07-28 00:22:06 +0200

    Script: get_youtube_url and youtube_url_validation

--- bin/tests/test.py
+++ bin/tests/test.py
@@ -14,6 +14,70 @@ import sys
 sys.path.append('.')
 target = __import__("vallenato_fr")
 
+# Used to test manual entry
+mock_raw_input_counter = 0
+mock_raw_input_values = []
+def mock_raw_input(s):
+    global mock_raw_input_counter
+    global mock_raw_input_values
+    mock_raw_input_counter += 1
+    return mock_raw_input_values[mock_raw_input_counter - 1]
+target.input = mock_raw_input
+
+class TestGetYoutubeUrl(unittest.TestCase):
+    def test_get_youtube_url_valid(self):
+        # Pass it a valid YouTube URLs
+        global mock_raw_input_counter
+        global mock_raw_input_values
+        mock_raw_input_counter = 0
+        mock_raw_input_values = ["https://youtu.be/oPEirA4pXdg"]
+        (video_id, video_url) = target.get_youtube_url("")
+        self.assertEqual(video_id, "oPEirA4pXdg")
+        self.assertEqual(video_url, "https://www.youtube.com/watch?v=oPEirA4pXdg")
+
+
+    def test_get_youtube_url_invalid_then_valid(self):
+        # Pass it a valid YouTube URLs
+        global mock_raw_input_counter
+        global mock_raw_input_values
+        mock_raw_input_counter = 0
+        mock_raw_input_values = ["ZZZ", "abc", "https://youtu.be/oPEirA4pXdg"]
+        (video_id, video_url) = target.get_youtube_url("")
+        self.assertEqual(video_id, "oPEirA4pXdg")
+        self.assertEqual(video_url, "https://www.youtube.com/watch?v=oPEirA4pXdg")
+
+    def test_get_youtube_url_quit(self):
+        # Entering "q" will make the script exit with value 10
+        global mock_raw_input_counter
+        global mock_raw_input_values
+        mock_raw_input_counter = 0
+        mock_raw_input_values = ["q"]
+        with self.assertRaises(SystemExit) as cm:
+            (video_id, video_url) = target.get_youtube_url("")
+        the_exception = cm.exception
+        self.assertEqual(the_exception.code, 10)
+
+class TestYoutubeUrlValidation(unittest.TestCase):
+    def test_youtube_url_validation_true(self):
+        valid_youtube_urls = [
+            "http://www.youtube.com/watch?v=oPEirA4pXdg",
+            "http://youtu.be/oPEirA4pXdg",
+            "http://www.youtube.com/embed/oPEirA4pXdg?rel=0\" frameborder=\"0\"",
+            "https://www.youtube-nocookie.com/v/oPEirA4pXdg?version=3&amp;hl=en_US",
+            "https://www.youtube.com/watch?v=oPEirA4pXdg&list=PLZiwcAOEEWBqhsycvvZnBrz5Sw3yXCyRB&index=2&t=0s"
+        ]
+        for url in valid_youtube_urls:
+            self.assertTrue(target.youtube_url_validation(url))
+
+    def test_youtube_url_validation_false(self):
+        invalid_youtube_urls = [
+            "http://www.youtube.com/",
+            "http://www.youtube.com/?feature=ytca",
+            "https://www.youtube.com/channel/UC_8R235jg1ld6MCMOzz2khQ"
+        ]
+        for url in invalid_youtube_urls:
+            self.assertFalse(target.youtube_url_validation(url))
+
 class TestInitMain(unittest.TestCase):
     def test_init_main_no_arguments(self):
         """
@@ -23,6 +87,11 @@ class TestInitMain(unittest.TestCase):
         target.__name__ = "__main__"
         # Pass it no arguments
         target.sys.argv = ["scriptname.py"]
+        # Pass it two valid YouTube URLs
+        global mock_raw_input_counter
+        global mock_raw_input_values
+        mock_raw_input_counter = 0
+        mock_raw_input_values = ["http://www.youtube.com/watch?v=oPEirA4pXdg", "https://www.youtube.com/watch?v=q6cUzC6ESZ8"]
         # Run the init(), nothing specific should happen, the program exits correctly
         target.init()
 
--- bin/vallenato_fr.py
+++ bin/vallenato_fr.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 import sys
+import re
 
-# Retrieve the information of the new tutorial
-    # ID of the YouTube tutorial video
-        # Extract video ID and Normalize URL
-    # ID of the YouTube full video
-        # Extract video ID and Normalize URL
+def get_tutorial_info():
+    """Retrieve the information of the new tutorial"""
+    # What is the YouTube tutorial video?
+    (tutorial_id, tutorial_url) = get_youtube_url("tutorial")
+    # What is the YouTube full video?
+    (full_video_id, full_video_url) = get_youtube_url("full")
     # Song name
         # Autodetect, correct if needed
     # Author name
@@ -15,7 +17,34 @@ import sys
         # Autodetect, correct if needed
         # Check if slug not already used
 
-# Extract video ID and Normalize URL
+def get_youtube_url(type):
+    """Extract video ID and Normalize URL"""
+    video_id = None
+    video_url = None
+    s = input("Enter the ID or URL of the %s video ('q' to quit): " % type)
+    while not video_id:
+        if s.lower() == "q":
+            print("Exiting...")
+            sys.exit(10)
+        video_id = youtube_url_validation(s)
+        if not video_id:
+            s = input("Invalid %s video URL, please try again ('q' to quit): " % type)
+    video_url = "https://www.youtube.com/watch?v=%s" % video_id
+    # print(video_id, video_url)
+    return (video_id, video_url)
+
+def youtube_url_validation(url):
+    """Check that it is a valid YouTube URL.
+    Inspired from https://stackoverflow.com/a/19161373
+    """
+    youtube_regex = (
+        r'(https?://)?(www\.)?'
+        '(youtube|youtu|youtube-nocookie)\.(com|be)/'
+        '(watch\?v=|embed/|v/|.+\?v=)?([a-zA-Z0-9_-]{11})')
+    youtube_regex_match = re.match(youtube_regex, url)
+    if youtube_regex_match:
+        return youtube_regex_match.group(6)
+    return youtube_regex_match
 
 # Download the videos
     # Tutorial video
@@ -36,7 +65,7 @@ import sys
     # Link to the author's YouTube channel
 
 def main():
-    pass
+    get_tutorial_info()
 
 def init():
     if __name__ == "__main__":
