a8903f3cf13162b714594ec23fe14fd2e7ba88c2
Emilien Klein <emilien@klein.st>
2019-10-19 22:19:48 +0200

    W: Handle non-root page initialization

--- website/src/script.js
+++ website/src/script.js
@@ -18,7 +18,7 @@ function main(){
   document.getElementById('video_overlay_close').addEventListener("click", close_current_overlay);
 
   // Check if we started with another page than the root
-  //TODO
+  check_initial_page();
 }
 
 function populateMapAndList(mymap){
@@ -36,14 +36,14 @@ function populateMapAndList(mymap){
       };
       var marker = L.marker([locations[loc].latitude, locations[loc].longitude], markerOptions).addTo(mymap);
       marker._icon.id = "location_marker_" + i;
-      marker.addEventListener("click", function(){ overlay_on("list_overlay", loc); });
+      marker.addEventListener("click", function(){ update_history_for_location(loc); show_location_overlay(loc); });
       marker.addEventListener("mouseover", function(evt){ marker_hover(evt.target._icon.id, "highlighted"); });
       marker.addEventListener("mouseout", function(evt){ marker_hover(evt.target._icon.id, ""); });
 
       // Update list on the right side
       var li = document.createElement('li');
       li.id = "location_list_" + i;
-      li.addEventListener("click", function(){ overlay_on("list_overlay", loc); });
+      li.addEventListener("click", function(){ update_history_for_location(loc); show_location_overlay(loc); });
       ul.appendChild(li);
       li.innerHTML = loc;
 
@@ -53,34 +53,88 @@ function populateMapAndList(mymap){
   }
 }
 
-function overlay_on(id, loc) {
-  overlay_show(id);
-  document.getElementById("list_location").innerHTML = loc;
+function check_initial_page() {
+  if("" === window.location.hash){
+    // Nothing to do, it's the home page
+  } else {
+    // Identify for which page this slug is
+    var slug_found = false;
+    // Check if it's a location's slug
+    for (let loc in locations) {
+      if (locations.hasOwnProperty(loc)) {
+        if("#" + locations[loc].slug === window.location.hash){
+          slug_found = true;
+          show_location_overlay(loc);
+          break;
+        }
+      }
+    }
 
+    if (!slug_found) {
+      // Check if it's an individual video's slug
+      for (let loc in locations) {
+        if (locations.hasOwnProperty(loc)) {
+          for (let vid in locations[loc]["videos"]) {
+            if (locations[loc]["videos"].hasOwnProperty(vid)) {
+              let vid_id = locations[loc]["videos"][vid].id;
+              let vid_title_slug = locations[loc]["videos"][vid].slug;
+              if("#" + vid_title_slug + "/" + vid_id === window.location.hash){
+                slug_found = true;
+                // First load that location's overlay, to handle closing the video overlay nicely
+                show_location_overlay(loc);
+                // Load that page's video
+                // Note: autoplay will likely be disabled by the browser, as it won't have detected an explicit user action
+                play_video(vid_id, vid_title_slug);
+                break;
+              }
+            }
+          }
+          if(slug_found) {
+            break;
+          }
+        }
+      }
+    }
+
+    if (!slug_found) {
+      console.log("Invalid slug, redirecting to home page!");
+      // TODO: redirect to the root page
+    }
+  }
+}
+
+function update_history_for_location(loc) {
   // Update the URL for that location
-  history.pushState({}, loc, "#" + locations[loc]["slug"]);
+  history.pushState({"type": "location", "loc": loc}, loc, "#" + locations[loc]["slug"]);
+}
+
+function show_location_overlay(loc) {
+  overlay_show("list_overlay");
+  document.getElementById("list_location").innerHTML = loc;
 
   var ul = document.getElementById("list_videos");
   // Empty the list, if populated before
   ul.innerHTML = "";
 
   for (let vid in locations[loc]["videos"]) {
-    // Show all the videos taken at that location
-    var li = document.createElement('li');
-    ul.appendChild(li);
-    let vid_id = locations[loc]["videos"][vid].id;
-    vid_title = locations[loc]["videos"][vid].title;
-    let vid_title_slug = locations[loc]["videos"][vid].slug;
-    vid_thumbnail_url = locations[loc]["videos"][vid].thumbnail.url;
-    li.innerHTML = "<p id='vid_title_" + vid_id + "'>" + vid_title + "</p><img id='vid_img_" + vid_id + "' src='" + vid_thumbnail_url + "'/>"
-    // Handle clicks on the title or the image to play the video
-    document.getElementById("vid_title_" + vid_id).addEventListener("click", function(){ play_video(vid_id, vid_title_slug); });
-    document.getElementById("vid_img_" + vid_id).addEventListener("click", function(){ play_video(vid_id, vid_title_slug); });
+    if (locations[loc]["videos"].hasOwnProperty(vid)) {
+      // Show all the videos taken at that location
+      var li = document.createElement('li');
+      ul.appendChild(li);
+      let vid_id = locations[loc]["videos"][vid].id;
+      vid_title = locations[loc]["videos"][vid].title;
+      let vid_title_slug = locations[loc]["videos"][vid].slug;
+      vid_thumbnail_url = locations[loc]["videos"][vid].thumbnail.url;
+      li.innerHTML = "<p id='vid_title_" + vid_id + "'>" + vid_title + "</p><img id='vid_img_" + vid_id + "' src='" + vid_thumbnail_url + "'/>"
+      // Handle clicks on the title or the image to play the video
+      document.getElementById("vid_title_" + vid_id).addEventListener("click", function(){ play_video(vid_id, vid_title_slug); });
+      document.getElementById("vid_img_" + vid_id).addEventListener("click", function(){ play_video(vid_id, vid_title_slug); });
+    }
   }
 }
 
 function overlay_show(id) {
-  // Hide the overlay
+  // Show the overlay
   document.getElementById(id).style.display = "block";
 }
 
@@ -96,7 +150,7 @@ function marker_hover(marker_id, highlight) {
 
 function play_video(id, title_slug) {
   // Update the URL for that video
-  history.pushState({}, id, "#" + title_slug + "/" + id);
+  history.pushState({"type": "video", "id": id, "title_slug": title_slug}, id, "#" + title_slug + "/" + id);
   // Update the video overlay with the url for this video
   document.getElementById("video_overlay_iframe_placeholder").innerHTML = `<iframe id="video_overlay_iframe" width="560" height="315" src="https://www.youtube.com/embed/` + id + `?autoplay=1" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`
   // Show the overlay containing the YouTube Embed iframe
