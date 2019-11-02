9ebd94d6240c0025afb8fa0fab997ff2d3914083
Emilien Klein <emilien@klein.st>
2019-11-02 19:04:09 +0100

    Refresh production website before uploading

--- upload.sh
+++ upload.sh
@@ -2,5 +2,7 @@ echo "======== Uploading repo to Github ======== "
 git push
 echo "======== Uploading /aprender ======== "
 cd aprender && rsync -av --delete -e 'ssh -p 21324' --exclude=temp/ ./* e2jk@pascal.klein.st:/var/www/html/vallenato.fr/aprender
+echo "======== Refreshing www ======== "
+cd ../bin && python3 vallenato_fr.py --website --dump-uploaded-videos
 echo "======== Uploading www ======== "
 cd ../website/prod && rsync -av --delete -e 'ssh -p 21324' ./* e2jk@pascal.klein.st:/var/www/html/vallenato.fr
