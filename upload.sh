e52eec230b80208ba64d2ed26eb4fd94c50d7aed
Emilien Klein <emilien@klein.st>
2019-11-02 12:56:27 +0100

    Update upload script to publish the main Vallenato.fr website

--- upload.sh
+++ upload.sh
@@ -1,1 +1,6 @@
-git push && cd aprender && rsync -av --delete -e 'ssh -p 21324' --exclude=temp/ ./* e2jk@pascal.klein.st:/var/www/html/vallenato.fr/aprender
+echo "======== Uploading repo to Github ======== "
+git push
+echo "======== Uploading /aprender ======== "
+cd aprender && rsync -av --delete -e 'ssh -p 21324' --exclude=temp/ ./* e2jk@pascal.klein.st:/var/www/html/vallenato.fr/aprender
+echo "======== Uploading www ======== "
+cd ../website/prod && rsync -av --delete -e 'ssh -p 21324' ./* e2jk@pascal.klein.st:/var/www/html/vallenato.fr
